b1d18b3f710f8c14b08e587d50689a2280a9066b
APeng <522731323@qq.com>
2019-06-12 21:45:45 +0800

    前端初步搭建

---
+++ frontend/src/modules/home/views/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    HelloWorld -- Home
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -16,13 +16,14 @@ function getPages() {
     let pages = {}
     pages[page_env] = {
         entry: resolveSrc('./src/modules/' + page_env + '/main.js'),
-        template: resolveSrc('./src/modules/' + page_env + '/index.html')
+        template: resolveSrc('./src/modules/' + page_env + '/index.html'),
+        filename: resolveSrc(`./dist/${page_env}/index.html`)
     }
     return pages
 }
 
 module.exports = {
     outputDir: resolveSrc(`./dist/${page_env}`),
-    publicPath: page_env,   
+    publicPath: './',   
     pages: getPages()
 }
