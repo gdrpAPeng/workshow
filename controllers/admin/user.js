ea9018740f0dfcb2b7f34f8e0d603370fea6b3cb
gdrpAPeng <522731323@qq.com>
2019-06-21 15:05:33 +0800

    测试 xlsx

--- controllers/admin/user.js
+++ controllers/admin/user.js
@@ -1,43 +1,45 @@
-const fs = require('fs')
-const path = require('path')
-const xlsx = require('xlsx')
-const uploadPath = path.resolve(__dirname, '../../upload')
-
+const fs = require("fs");
+const path = require("path");
+const xlsx = require("xlsx");
+const uploadPath = path.resolve(__dirname, "../../upload");
 
 class User {
-    async uploadExcel(ctx, next) {
-        const file = ctx.request.files.file
-        const reader = fs.createReadStream(file.path)
-        const [ name, ext ] = file.name.split('.')
-        const time = new Date().getTime()
-        const filePath = `${uploadPath}/${name}_${time}.${ext}`
-        console.log(filePath)
-        console.log(__dirname)
-        const uploadStream = fs.createWriteStream(filePath)
-        
-        await reader.pipe(uploadStream)
-        const stat = fs.statSync(filePath)
-        
-        // 存在 则 说明文件上传成功
-        if(stat.isFile()) {
-            const workbook = xlsx.readFile(filePath)
-            
-            const result = this.excelToJson(workbook)
+  async uploadExcel(ctx, next) {
+    const file = ctx.request.files.file;
+    const reader = fs.createReadStream(file.path);
+    const [name, ext] = file.name.split(".");
+    const time = new Date().getTime();
+    const filePath = `${uploadPath}/${name}_${time}.${ext}`;
+    console.log(filePath);
+    console.log(__dirname);
+    const uploadStream = fs.createWriteStream(filePath);
 
-            // ctx.body = result
-        }
-    }
+    // await reader.pipe(uploadStream);
 
-    excelToJson(workbook) {
-        let result = {}
-        let sheetNames = workbook.SheetNames
-        SheetNames.forEach(function(sheetName) {
-            var worksheet = workbook.Sheets[sheetName]
-            result[sheetName] = xlsx.utils.sheet_add_json(worksheet)
-        })
-        return result
+    const getRes = await getFile(reader, uploadStream);
+
+    // const stat = fs.statSync(filePath);
+
+    // 存在 则 说明文件上传成功
+    if (!getRes) {
+      const workbook = xlsx.readFile(filePath);
+      const sheetNames = workbook.SheetNames;
+      sheetNames.forEach(function(sheetName) {
+        var worksheet = workbook.Sheets[sheetName];
+        let data = xlsx.utils.sheet_to_json(worksheet)
+        ctx.body = data
+      });
     }
-    
+  }
 }
 
-module.exports = new User()
+function getFile(reader, uploadStream) {
+  return new Promise(function(result) {
+    let stream = reader.pipe(uploadStream);
+    stream.on("finish", function(err) {
+      result(err);
+    });
+  });
+}
+
+module.exports = new User();
